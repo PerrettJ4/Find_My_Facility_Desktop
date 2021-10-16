17e6789151e2ba70c43f9da944190314d86917d1
perrettJ4 <perrettj4@gmail.com>
2021-10-16 17:44:17 +0100

    added caching in local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import "./App.css";
 import Switcher from "./Tile";
+import css from "./App.module.css";
 // import { useGetSports } from "./../hooks/";
 // import React, { useEffect, useReducer } from "react";
 // import reducer from "./reducer";
 
 function App() {
   //
-  return <Switcher />;
+  return (
+    <div className={css.App}>
+      <Switcher />
+    </div>
+  );
 }
 
 export default App;
